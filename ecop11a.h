adc3d8f49bba50a262de0168e04995b3b39ce8c8
Aurelianogbf <d2025006474@unifei.edu.br>
2025-05-12 12:28:19 +0000

    fiz as pastas

---
+++ ecop11a.h
@@ -0,0 +1,10 @@
+#ifndef ECOP11A_H
+#define ECOP11A_H
+
+float cubo(float aresta);
+float paralelepipedo(float largura, float profundidade, float altura);
+float esfera(float raio);
+float cilindro(float raio, float altura);
+float piramide(float largura, float profundidade, float altura);
+
+#endif /* ECOP11A_H */
--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include "ecop11a.h"
 
 int main(void)
 {
-    printf("Ola unifei!\n");
+    printf("Olá UNIFEI!\n");
+
+    float volume = esfera(3.0);
+    printf("Volume da esfera: %.2f\n", volume);
+
     return 0;
 }
